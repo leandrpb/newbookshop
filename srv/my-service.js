2ba66e860626c3b36ddba710ff45be5bb97202e3
leandrpb <leandro.hero@gmail.com>
2023-08-10 18:15:03 -0300

    RT

--- srv/my-service.js
+++ srv/my-service.js
@@ -2,11 +2,12 @@
  * Implementation for CatalogService defined in ./cat-service.cds
  */
 const cds = require('@sap/cds')
+const {Books} = cds.entities
 
 module.exports = (srv)=>{
 
     // Use reflection to get the csn definition of Books
-    const {Books} = cds.entities
+
 
     //Add some discount if overstocked item
     srv.after ('READ', 'Books', (each)=>{
@@ -42,17 +43,20 @@ module.exports = (srv)=>{
 
 async function _reduceStock(req) {
 
-    const tx = cds.transaction(req), order = req.data;
+    const { Items : orderItems } = req.data
 
-    if (order.Items) {
+    return cds.transaction(req) .run (()=> orderItems.map (item => 
+        UPDATE (Books)
+            .set ('stock -=', item.amount)
+            .where ('ID =', item.book_ID) .and ('stock >=', item.amount)
+        ))
+        
+        .then (all => all.array.forEach ((affectedRows,i) => {
 
-        const affectedRows = await tx.run(order.Items.map(item =>
-                 UPDATE(Books) .where({ID:item.book_ID})
-                               .and(`stock >= `, item.amount)
-                               .set(`stock -= `, item.amount)
-                )
-            )
+               if (affectedRows === 0) {
+                req.error (409, `${orderItems[i].amount} exceeds stock for book #${orderItems[i].book_ID}`)
+               }
 
-         if (affectedRows.some(row => !row)) req.error(409, 'Sold out, sorry!')
-    }
+            }
+        ))
 }
